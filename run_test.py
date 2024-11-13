52585edfbf58e359d32bf7823cd26406273f640b
방기남 <73218016+Bread-kn72@users.noreply.github.com>
2024-11-13 11:10:26 +0900

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from test_cases.test_case1 import test_case1
 from test_cases.test_case2 import test_case2
+from test_cases.test_case3 import test_case3
 
 driver = webdriver.Chrome()
 
