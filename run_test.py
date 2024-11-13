c4e2862ad58b0f1c0dd6d93e86783f55053350fd
방기남 <73218016+Bread-kn72@users.noreply.github.com>
2024-11-13 11:04:02 +0900

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from test_cases.test_case1 import test_case1, driver
+from test_cases.test_case1 import test_case1
 from test_cases.test_case2 import test_case2
 
 driver = webdriver.Chrome()
 
 try:
-    test_case1()
+    test_case1(driver)
     test_case2(driver, "김민재")
 
 finally:
