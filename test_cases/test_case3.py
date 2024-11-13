7434584968db7358fcef064e7429f99538c91d9d
방기남 <73218016+Bread-kn72@users.noreply.github.com>
2024-11-13 11:23:31 +0900

    Update test_case3.py

--- test_cases/test_case3.py
+++ test_cases/test_case3.py
@@ -1,45 +1,7 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import time
-
-driver = webdriver.Chrome()
     
-try:
-    # 사장님 사이트 진입
-    driver.get('https://ceo.monki.net/users/login')
-    time.sleep(2)
-
-    #유저타입 선택
-    dropdown = Select(driver.find_element(By.ID, "user_type"))
-    dropdown.select_by_value("type_003")
-    driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(2)
-    
-    #아이디 입력
-    id = driver.find_element(By.ID, "user_id")
-    id.clear()
-    id.send_keys("monkistore1" + Keys.TAB)
-    time.sleep(1)
-
-    #비밀번호 입력
-    password = driver.find_element(By.ID, "user_pass")
-    password.clear()
-    password.send_keys("test123!")
-
-    #자동 로그인 상태 체크
-    auto_login = driver.find_element(By.ID, "chkRemember")
-
-    if not auto_login.is_selected():
-        auto_login.click()
-
-    #로그인 시도
-    time.sleep(1)
-    driver.find_element(By.ID, "btnLogin").click()
-
-    time.sleep(10)
-
-finally:
-    driver.quit()
+def test_case3(driver):
+    print("Test case3 완료")
 
