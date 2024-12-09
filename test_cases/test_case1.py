fcd052ff45708064b2bcc9ef7272d4b14c738807
Bread-kn72 <ordinarytom72@gmail.com>
2024-12-09 23:46:40 +0900

    delete py

--- test_cases/test_case1.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def test_case1(driver):
-    driver.get("https://www.google.com")
-    time.sleep(2)
-    print("test case1 완료")
--- test_cases/test_case3.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-    
-def test_case3(driver):
-    print("Test case3 완료")
-
